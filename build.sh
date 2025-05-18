0394132bd1d20f39a46e3c047611b09d4124fd82
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-05-18 18:31:25 +0200

    wip

---
+++ build.sh
@@ -0,0 +1,16 @@
+rm -rf build
+rm -rf conan.lock
+
+
+conan lock create conanfile.py --version "1.0" --lockfile=conan.lock --lockfile-out=build/conan.lock
+conan install conanfile.py --lockfile=build/conan.lock -of build --build=missing
+
+cmake --preset conan-release \
+         -DCMAKE_VERBOSE_MAKEFILE=ON \
+         -DGLOBAL_BUILD=ON \
+         -DCMAKE_BUILD_TYPE=Release
+
+cmake --build --preset conan-release -j4 \
+         -DCMAKE_VERBOSE_MAKEFILE=ON \
+         -DGLOBAL_BUILD=ON \
+         -DCMAKE_BUILD_TYPE=Release
