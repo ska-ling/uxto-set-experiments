6ec531bb66586c315f4a4e07fa558375a3667d7f
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-06-12 10:46:46 +0200

    wip

--- src/leveldb_v1.hpp
+++ src/leveldb_v1.hpp
@@ -25,6 +25,18 @@ namespace fs = std::filesystem;
 using utxo_key_t = std::array<uint8_t, 36>;
 using span_bytes = std::span<uint8_t const>;
 
+void print_key(utxo_key_t const& key) {
+    // first 32 bytes are the transaction hash, print in hex reversed
+    for (size_t i = 0; i < 32; ++i) {
+        log_print("{:02x}", key[31 - i]);
+    }   
+    // the last 4 bytes are the output index, print as integer
+    uint32_t output_index = 0;
+    std::copy(key.end() - 4, key.end(), reinterpret_cast<uint8_t*>(&output_index));
+    log_print(":{}", output_index);
+    log_print("\n");
+}
+
 // Utility function to create leveldb::Slice from utxo_key_t
 inline leveldb::Slice key_to_slice(utxo_key_t const& key) {
     return leveldb::Slice(reinterpret_cast<char const*>(key.data()), key.size());
